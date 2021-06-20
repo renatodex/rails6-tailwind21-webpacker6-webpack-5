e0bb16523ec14109a6faf2e612de608c2ad23985
renatodex <renatodex@gmail.com>
2021-06-20 02:01:26 -0300

    Revert postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('tailwindcss'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
@@ -9,6 +8,7 @@ module.exports = {
       },
       stage: 3
     }),
+    require('tailwindcss'),
     require('autoprefixer')
   ]
 }
